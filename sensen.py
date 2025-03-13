dba795c8198a2988020e45ab3324203972e63c8e
sen <329911810@qq.com>
2025-03-13 23:43:01 +0800

    first commit by sen

---
+++ sensen.py
@@ -0,0 +1,15 @@
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')#这个是单行注释
+print('hello world')
+'''我是多行注释 
+第二行注释1
+'''
+# 输出函数print()
+print('哈哈哈','嘿嘿嘿','嘻嘻嘻',sep='.')
+print('hello',end='8' )
+print('sensen')
+print('dasda')
